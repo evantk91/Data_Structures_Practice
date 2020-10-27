400b9083632383da86f6171fb1fcaa687ba80480
etgreer91 <etgreer91@gmail.com>
2020-10-27 11:57:06 -0600

    updates tree practice

--- trees.js
+++ trees.js
@@ -48,12 +48,78 @@ class Tree {
     }
 }
 
-let node1 = new Node(1)
-let node2 = new Node(2)
-let node3 = new Node(3) 
+class Stack {
+    constructor() {
+       this.list = [];
+    }
 
-let tree = new Tree(1)
+    push = function(value) {
+       this.list.push(value);
+    }
 
-console.log(node1.setLeftChild(node2))
-console.log(node1.setRightChild(node3))
-console.log(tree)
+    pop = function() {
+       return this.list.pop();
+    }
+
+    top = function() {
+       if(this.list.length > 0) {
+          return this.list[this.list.length - 1]    
+       } else {
+          return null; 
+       }
+    }
+
+    isEmpty = function() {
+       return this.list.length === 0; 
+    }
+}
+
+function preOrderDFS(tree) {
+    let visitOrder = [];
+    let stack = new Stack();
+    let node = tree.getRoot();
+
+    stack.push(node);
+    node = stack.top();
+    visitOrder.push(node.getValue());
+
+    let count = 0;
+    let loopLimit = 7;
+    while(node) {
+        count += 1;
+        if(node.hasLeftChild()) {
+           node = node.getLeftChild();
+           //add to top of stack
+           stack.push(node);
+           node = stack.top();
+           //add to visit order
+           visitOrder.push(node.getValue());
+        } else if(node.hasRightChild()) {
+           node = node.hasRightChild();
+           //add to top of stack
+           stack.push(node);
+           node = stack.top();
+           //add to visit order
+           visitOrder.push(node.getValue());
+        } else {
+           stack.pop()
+           if(!stack.isEmpty()) {
+              node = stack.top()
+           } else {
+              node = null; 
+           }
+        }
+    }
+
+    return visitOrder;
+}
+
+let tree = new Tree('apple')
+let node2 = new Node('banana')
+tree.getRoot().setLeftChild(node2)
+let node3 = new Node('cherry')
+tree.getRoot().setRightChild(node3)
+let node4 = new Node('dates')
+tree.getRoot().getLeftChild().setLeftChild(node4) 
+
+console.log(preOrderDFS(tree))
